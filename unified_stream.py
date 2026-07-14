69a600efa5bdb29d3db12792ba0395a5fd0248c7
Ujjawal Kumar <107112307+affnarayani@users.noreply.github.com>
2026-07-14 20:54:57 +0530

    14072026

--- unified_stream.py
+++ unified_stream.py
@@ -83,8 +83,8 @@ def start_pipeline():
             'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
             'extractor_args': {
                 'youtube': {
-                    # Alag-alag clients ka combination try karein
-                    'player_client': ['ios', 'android', 'web'],
+                    'player_client': 'web',
+                    'skip': ['dash', 'hls']
                 }
             },
             'quiet': False,
